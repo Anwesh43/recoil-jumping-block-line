fcab8c588f6f330664b5f763bd7eabcf8b35286d
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 16:40:47 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -22,3 +22,66 @@ export const useAnimatedScale = (scGap, delay) => {
         }
     }
 }
+
+export const useDimension = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            window.onresize = () => {
+
+            }
+        }
+    })
+}
+
+const maxScale = (scale, i, n) => Math.max(0, scale - i / n)
+
+const divideScale = (scale, i, n) => Math.min(1 / n, maxScale(scale, i, n)) * n  
+
+export const useStyle = (w, h, scale) => {
+    const sf = Math.sin(Math.PI * scale)
+    const sf1 = divideScale(sf, 0, 2)
+    const sf2 = divideScale(sf, 1, 2)
+    const fixedY = 0.9 * h 
+    const size = Math.min(w, h) / 10
+    const position = 'absolute'
+    const x = w * sf 
+    const y = fixedY - size + (fixedY - size) * 0.5 * (sf2 - sf1) 
+    const background = 'indigo'
+    const blockX = (w - size) * sf 
+    return {
+        lineStyle() {
+            const left = `0px`
+            const top = `${fixedY}px`
+            const width = `${x}px`
+            const height = `${size / 8}px`
+            return {
+                position, 
+                left,
+                top, 
+                background, 
+                width, 
+                height 
+            }
+        },
+        blockStyle() {
+            const left = `${blockX}px`
+            const top = `${y}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            return {
+                position, 
+                left, 
+                top, 
+                width, 
+                height, 
+                background 
+            }
+        }
+    }
+}
